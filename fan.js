357af6e2c5dec0aea4ccfda5467039670ccd0231
gbjbuobbl <79749963+gbjbuobbl@users.noreply.github.com>
2021-11-26 16:56:48 +0530

    Add files via upload

---
+++ fan.js
@@ -0,0 +1,26 @@
+status= "";
+
+function preload() {
+    loadImage("fan.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 150);
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "Status = Detecting Objects"
+  }
+  
+  function modelLoaded(){
+        console.log("model loaded");
+        status = true;
+        objectDetector.detect(img , gotResult);
+  }
+
+  function gotResult(error,results) {
+    if(error) {
+      console.log(error);
+    }
+    console.log(results);
+    object = results;
+  }
